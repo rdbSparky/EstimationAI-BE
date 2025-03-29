dc111548332738f52d4537e0f64aa3e584b86c96
manavbehal-nickelfox <manav.behal@nickelfox.com>
2025-03-29 14:30:52 +0530

    build.sh

--- estimation_ai/settings.py
+++ estimation_ai/settings.py
@@ -36,8 +36,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['estimationai-be.onrender.com', 'localhost']
 
 # Application definition
 
